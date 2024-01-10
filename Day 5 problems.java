6861edd84f19b8fe9db70644f001e35b85fc6333
Arushi Tandon <135385191+ArushiTandon@users.noreply.github.com>
2024-01-10 22:17:53 +0530

    Add files via upload

---
+++ Day 5 problems.java
@@ -0,0 +1,34 @@
+Day 5 problems
+Problem 1 : 238. Product of Array Except Self
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        Arrays.fill(ans, 1);
+        int curr = 1;
+        for(int i = 0; i < n; i++) {
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+        curr = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+        return ans;
+    }
+}
+
+---------------------------------------------------------------------
+Problem 2: 88. Merge Sorted Arrays
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
